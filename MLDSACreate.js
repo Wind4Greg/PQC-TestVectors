03734820ba6b0f46056cc7b789476d1c89152f89
Wind4Greg <gregb@grotto-networking.com>
2026-01-25 11:44:49 -0800

    Work through key encodings. First working version of ML-DSA44 based test vector generation. No verification yet.

--- MLDSACreate.js
+++ MLDSACreate.js
@@ -13,6 +13,8 @@ import { sha256 } from '@noble/hashes/sha2.js';
 import * as utils from '@noble/hashes/utils.js';
 const { bytesToHex, hexToBytes } = utils;
 import { proofConfig, transform, hashing } from './DIUtils.js';
+import { ml_dsa44, ml_dsa65, ml_dsa87 } from '@noble/post-quantum/ml-dsa.js';
+import { base64url } from 'multiformats/bases/base64'
 
 // General scheme parameters
 const canonScheme = "rdfc";
@@ -20,22 +22,26 @@ const hash = "sha256";
 
 
 const dirsAndFiles = {
-  outputDir: './output/ecdsa-rdfc-2019-p256/employ/',
-  inputFile: './input/employmentAuth.json',
-  proofOptionsFile: './input/proofOptions.json'
+  outputDir: './output/mldsa44-rdfc-2024/alumni/',
+  inputFile: './input/unsigned.json',
+  proofOptionsFile: './input/proofOptions.json',
+  keyFile: './input/KeysMLDSA.json'
+
 }
 
 // Create output directory for the results
 const baseDir = dirsAndFiles.outputDir;
 let status = await mkdir(baseDir, {recursive: true});
 
-const keyPair = {
-    publicKeyMultibase: "zDnaepBuvsQ8cpsWrVKw8fbpGpvPeNSjVPTWoq6cRqaYzBKVP"
-};
-
-
-let privateKey = hexToBytes("C9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721");
-let publicKey = P256.getPublicKey(privateKey);
+// TODO MLDSA44 keys
+let allKeys = JSON.parse(
+    await readFile(
+      new URL(dirsAndFiles.keyFile, import.meta.url)
+    )
+  );
+const publicKeyMultibase = allKeys.mldsa44.publicKeyMultibase;
+let secretKey = hexToBytes(allKeys.mldsa44.secretKeyHex);
+let publicKey = hexToBytes(allKeys.mldsa44.publicKeyHex);
 
 // Read input document from a file or just specify it right here.
 let document = JSON.parse(
@@ -48,7 +54,7 @@ let document = JSON.parse(
 
 // Transform the document
 let docCannon = await transform(document, canonScheme);
-writeFile(baseDir + 'transformDocECDSAP256.txt', docCannon);
+writeFile(baseDir + 'transformDocMLDSA44.txt', docCannon);
 
 
 // Set proof options
@@ -58,45 +64,44 @@ let proofOptions = JSON.parse(
     )
   );
 // Must specify cryptosuite
-proofOptions.cryptosuite = "ecdsa-rdfc-2019";
+proofOptions.cryptosuite = "mldsa44-rdfc-2019";
 // Must provide verification methods related to public key
-proofOptions.verificationMethod = 'did:key:' + keyPair.publicKeyMultibase + '#'
-  + keyPair.publicKeyMultibase;
+proofOptions.verificationMethod = 'did:key:' + publicKeyMultibase + '#'
+  + publicKeyMultibase;
 
 proofOptions["@context"] = document["@context"];
 // Proof Configuration
 let proofCanon = await proofConfig(proofOptions, canonScheme);
 console.log("Proof Configuration Canonized:");
-console.log(proofCanon);
-writeFile(baseDir + 'proofCanonECDSAP256.txt', proofCanon);
+// console.log(proofCanon);
+writeFile(baseDir + 'proofCanonMLDSA44.txt', proofCanon);
 
 // Hashing
 let combinedHash = hashing(docCannon, proofCanon, hash);
-writeFile(baseDir + 'combinedHashECDSAP256.txt', bytesToHex(combinedHash));
+writeFile(baseDir + 'combinedHashMLDSA44.txt', bytesToHex(combinedHash));
 
 // Sign
-let msgHash = sha256(combinedHash); // Hash is done outside of the algorithm in noble/curve case.
-let signature = P256.sign(msgHash, privateKey,  { prehash: false, lowS: false });
-console.log("P256 based signature:")
-console.log(signature);
-writeFile(baseDir + 'sigHexECDSAP256.txt', bytesToHex(signature));
+let signature = ml_dsa44.sign(combinedHash, secretKey);
+console.log("MLDSA44 based signature:")
+console.log(signature.length);
+writeFile(baseDir + 'sigHexMLDSA44.txt', bytesToHex(signature));
 console.log("Computed Signature from private key:");
-console.log(base58btc.encode(signature));
-writeFile(baseDir + 'sigBTC58ECDSAP256.txt', base58btc.encode(signature));
+// console.log(base64url.encode(signature));
+writeFile(baseDir + 'sigBase64urlMLDSA44.txt', base64url.encode(signature));
 
 // Verify (just to see we have a good private/public pair)
-let pbk = base58btc.decode(keyPair.publicKeyMultibase);
+let pbk = base58btc.decode(publicKeyMultibase);
 pbk = pbk.slice(2, pbk.length); // First two bytes are multi-format indicator
-console.log(`Public Key hex: ${bytesToHex(pbk)}, Length: ${pbk.length}`);
-let result = P256.verify(signature, msgHash, pbk, { prehash: false, lowS: false });
+// console.log(`Public Key hex: ${bytesToHex(pbk)}, Length: ${pbk.length}`);
+let result = ml_dsa44.verify(signature, combinedHash, pbk);
 console.log(`Signature verified: ${result}`);
 
 // Construct Signed Document
 let signedDocument = Object.assign({}, document);
 delete proofOptions['@context'];
 signedDocument.proof = proofOptions;
-signedDocument.proof.proofValue = base58btc.encode(signature);
+signedDocument.proof.proofValue = base64url.encode(signature);
 
-console.log(JSON.stringify(signedDocument, null, 2));
-writeFile(baseDir + 'signedECDSAP256.json', JSON.stringify(signedDocument, null, 2));
+// console.log(JSON.stringify(signedDocument, null, 2));
+writeFile(baseDir + 'signedMLDSA44.json', JSON.stringify(signedDocument, null, 2));
 
